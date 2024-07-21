09f0059ff6d7f26f596b1415b497b40baf763459
seda <seda.diriker@gmail.com>
2024-07-21 13:50:44 +0300

    razor runtime version fixed

--- obj/Debug/net8.0/asp.net-mvc.AssemblyInfo.cs
+++ obj/Debug/net8.0/asp.net-mvc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("asp.net-mvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38816adcfef0c3ae54be811a000b9573d9debcf5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b2bdfe648e2a4d25917aa1341f74591189a71d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("asp.net-mvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("asp.net-mvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
