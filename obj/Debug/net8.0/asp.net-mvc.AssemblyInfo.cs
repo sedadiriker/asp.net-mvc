31d3997627009dae786d51a9cbd076fd5d867bc7
seda <seda.diriker@gmail.com>
2024-07-21 14:10:58 +0300

    Header and Footer renderpartialasync created

--- obj/Debug/net8.0/asp.net-mvc.AssemblyInfo.cs
+++ obj/Debug/net8.0/asp.net-mvc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("asp.net-mvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00cc99e44440070d70819a9cad2b2ccf5ff734f5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed4c5bddcdfa99cfe6ae18911afc8eb99c3b6c75")]
 [assembly: System.Reflection.AssemblyProductAttribute("asp.net-mvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("asp.net-mvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
